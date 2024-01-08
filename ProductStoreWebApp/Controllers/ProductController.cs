9842b514fe5424f8e129c01a0cf18609a490d19e
Shubhpanwar23 <shubhpanwar232@gmail.com>
2024-01-08 16:53:42 +0530

    tried to do add product

--- ProductStoreWebApp/Controllers/ProductController.cs
+++ ProductStoreWebApp/Controllers/ProductController.cs
@@ -27,4 +27,8 @@ public class ProductController : Controller
         Product prd = _svc.GetById(id);
         return View(prd);
     }
+    [HttpGet]
+     public IActionResult AddProduct(){
+        return View();
+    }
 }
