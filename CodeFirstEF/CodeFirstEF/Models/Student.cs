2724e2cc83a24df5cc6cb8462409a6a3f543fabe
suyog <suyogghate96@gmail.com>
2024-06-27 22:08:56 +0530

    made CRUD operations in EF with code first approach.

---
+++ CodeFirstEF/CodeFirstEF/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstEF.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("StudentName", TypeName = "varchar(25)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("StudentGender",TypeName = "varchar(25)")]
+        public string Gender { get; set; }
+
+        [Required]
+        public int? Age { get; set; }
+
+        [Required]
+        public int? Standard { get; set; }
+    }
+}
